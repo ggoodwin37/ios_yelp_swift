5caac55e86e3a64f77aea41a000e69979d9bd61f
gideong <gideong@yahoo-inc.com>
2015-09-04 16:11:22 -0700

    Version fixes.

--- Yelp/Business.swift
+++ Yelp/Business.swift
@@ -31,12 +31,12 @@ class Business: NSObject {
         var address = ""
         if location != nil {
             let addressArray = location!["address"] as? NSArray
-            var street: String? = ""
+            //var street: String? = ""
             if addressArray != nil && addressArray!.count > 0 {
                 address = addressArray![0] as! String
             }
             
-            var neighborhoods = location!["neighborhoods"] as? NSArray
+            let neighborhoods = location!["neighborhoods"] as? NSArray
             if neighborhoods != nil && neighborhoods!.count > 0 {
                 if !address.isEmpty {
                     address += ", "
@@ -50,10 +50,10 @@ class Business: NSObject {
         if categoriesArray != nil {
             var categoryNames = [String]()
             for category in categoriesArray! {
-                var categoryName = category[0]
+                let categoryName = category[0]
                 categoryNames.append(categoryName)
             }
-            categories = ", ".join(categoryNames)
+            categories = categoryNames.joinWithSeparator(",")
         } else {
             categories = nil
         }
@@ -76,10 +76,10 @@ class Business: NSObject {
         reviewCount = dictionary["review_count"] as? NSNumber
     }
     
-    class func businesses(#array: [NSDictionary]) -> [Business] {
+    class func businesses(array array: [NSDictionary]) -> [Business] {
         var businesses = [Business]()
         for dictionary in array {
-            var business = Business(dictionary: dictionary)
+            let business = Business(dictionary: dictionary)
             businesses.append(business)
         }
         return businesses
